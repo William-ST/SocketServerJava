61da86112b0a01b3c3ed9e7798c70055b73a77b2
william_ST <wsulcata@everis.com>
2017-05-27 18:54:31 -0500

    first commit

---
+++ src/socketserver_1/Provider.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socketserver_1;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author William_ST
+ */
+public class Provider {
+    ServerSocket providerSocket;
+    Socket connection = null;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    String message;
+    
+    Provider(){}   
+    
+    void run()
+    {
+        try{
+            //1. creating a server socket
+            providerSocket = new ServerSocket(2017, 10);
+            //2. Wait for connection
+            System.out.println("Waiting for connection");
+            connection = providerSocket.accept();
+            System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+            //3. get Input and Output streams
+            out = new ObjectOutputStream(connection.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(connection.getInputStream());
+            sendMessage("Connection successful");
+            //4. The two parts communicate via the input and output streams
+            do{
+                try{
+                    message = (String)in.readObject();
+                    System.out.println("client>" + message);
+                    if (message.equals("bye"))
+                        sendMessage("bye");
+                }
+                catch(ClassNotFoundException classnot){
+                    System.err.println("Data received in unknown format");
+                }
+            }while(!message.equals("bye"));
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            //4: Closing connection
+            try{
+                in.close();
+                out.close();
+                providerSocket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+    
+    void sendMessage(String msg) {
+        try {
+            out.writeObject(msg);
+            out.flush();
+            System.out.println("server>" + msg);
+        } catch(IOException ioException) {
+            System.err.println("ioException: "+ioException.toString());;
+        }
+    }
+    
+}
